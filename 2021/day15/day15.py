0c61fc4d26ca4afd547dd687a486aea03a8830ef
Samuel Fang <samuelfangjw@gmail.com>
2021-12-15 16:23:08 +0800

    Modify day15 to use A-star

--- 2021/day15/day15.py
+++ 2021/day15/day15.py
@@ -1,5 +1,7 @@
 import sys
 import heapq
+from collections import defaultdict
+import math
 
 file = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 grid = []
@@ -12,28 +14,38 @@ with open(file) as f:
 def get_neighbours(x,y):
     nbs = []
     for x1,y1 in [(x-1,y), (x, y-1), (x,y+1), (x+1, y)]:
-        if 0 <= x1 < rows and 0 <= y1 < cols and (x1,y1) not in visited:
+        if 0 <= x1 < rows and 0 <= y1 < cols and (x1,y1):
             nbs.append((x1, y1))
     return nbs
+
+def manhatten(i,j,x,y):
+    return abs(i-x) + abs(j-y)
 
 def solve():
     global rows, cols, visited
     pq = []
+    costs = defaultdict(lambda:math.inf)
     visited = set()
     rows = len(grid)
     cols = len(grid[0])
     end = (rows-1, cols-1)
+    heapq.heappush(pq, (0 + manhatten(0,0,rows-1,cols-1), (0,0)))
+    costs[(0,0)] = 0
     while pq:
-        c, (x,y) = heapq.heappop(pq)
+        _, (x,y) = heapq.heappop(pq)
         if (x,y) in visited:
             continue
         if (x,y) == end:
-            print(c)
+            print(costs[(x,y)])
             break
         visited.add((x,y))
         nbs = get_neighbours(x,y)
+        c = costs[(x,y)]
         for x1,y1 in nbs:
-            heapq.heappush(pq, (c + grid[x1][y1], (x1,y1)))
+            c1 = c + grid[x1][y1]
+            if c1 < costs[(x1,y1)]:
+                costs[(x1,y1)] = c1
+                heapq.heappush(pq, (costs[(x1,y1)] + manhatten(y1,x1,end[0],end[1]), (x1,y1)))
 
 solve()
 
