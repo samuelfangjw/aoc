6112619c0b00dc5992a01bf27c6fb240fba4b5a6
Samuel Fang <samuelfangjw@gmail.com>
2022-12-13 17:16:08 +0800

    Update 2022 day13

--- 2022/day13/day13.py
+++ 2022/day13/day13.py
@@ -11,28 +11,28 @@ with open(file) as f:
 def check(a, b):
     if isinstance(a, int) and isinstance(b, int):
         if a < b:
-            return 1
+            return -1
         elif a == b:
             return 0
         else:
-            return -1
+            return 1
     elif isinstance(a, int):
         a = [a]
     elif isinstance(b, int):
         b = [b]
 
-    m_len = min(len(a), len(b))
-    for i in range(m_len):
-        r = check(a[i], b[i])
+    # a and b are lists
+    for x, y in zip(a, b):
+        r = check(x, y)
         if r != 0:
             return r
 
     if len(a) < len(b):
-        return 1
+        return -1
     elif len(a) == len(b):
         return 0
     else:
-        return -1
+        return 1
 
 
 pkts = []
@@ -42,14 +42,14 @@ part1 = 0
 for idx, pair in enumerate(data.split('\n\n')):
     a, b = map(literal_eval, pair.strip().split('\n'))
     pkts.extend([a, b])
-    if check(a, b) == 1:
+    if check(a, b) == -1:
         part1 += idx + 1
 
 print(part1)
 
 # part 2
 pkts.extend([[[2]], [[6]]])
-pkts = sorted(pkts, key=cmp_to_key(check), reverse=True)
+pkts = sorted(pkts, key=cmp_to_key(check))
 part2 = (pkts.index([[2]]) + 1) * (pkts.index([[6]]) + 1)
 
 print(part2)
