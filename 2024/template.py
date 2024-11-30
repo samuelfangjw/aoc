07e9de835fd64635f1fb0bd5cf1da4172f09c58c
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2024-11-30 17:24:50 +0800

    Add 2024 template

---
+++ 2024/template.py
@@ -0,0 +1,19 @@
+import sys
+import itertools
+import re
+import math
+from copy import copy, deepcopy
+from statistics import mean, median
+from collections import Counter, defaultdict, deque
+
+file = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+data = []
+
+with open(file) as f:
+    for line in f:
+        line = line.strip()
+        # line = line.strip().split()
+        data.append(line)
+        # data.append([int(x) for x in line])
+
+print(data)
