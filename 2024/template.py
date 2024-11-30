1a1d7b29ff35d6800b973d7d81925e9f6eb8264f
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2024-12-01 02:28:36 +0800

    Update 2024 template

--- 2024/template.py
+++ 2024/template.py
@@ -8,9 +8,9 @@ from statistics import mean, median
 from collections import Counter, defaultdict, deque
 
 file = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
-data = []
 
 with open(file) as f:
+    data = []
     for line in f:
         line = line.strip()
         # line = line.strip().split()
