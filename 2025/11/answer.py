243e8a67d72b5db024fb764a013295136773393e
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2025-12-11 15:32:20 +0800

    Add 2025 day 11

---
+++ 2025/11/answer.py
@@ -0,0 +1,46 @@
+from collections import defaultdict
+from functools import cache
+import sys
+
+file = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+
+with open(file, "r") as f:
+    lines = [line.strip() for line in f.readlines()]
+
+
+nodes = defaultdict(list)
+for line in lines:
+    raw = line.split(":")
+    src = raw[0].strip()
+    dsts = [d.strip() for d in raw[1].strip().split()]
+    nodes[src].extend(dsts)
+
+visited = set()
+
+
+@cache
+def dfs(node, end):
+    if node == end:
+        return 1
+
+    if not nodes[node]:
+        return 0
+
+    visited.add(node)
+    res = 0
+    for n in nodes[node]:
+        if n not in visited:
+            res += dfs(n, end)
+    visited.remove(node)
+
+    return res
+
+
+part1 = dfs("you", "out")
+
+path1 = dfs("svr", "fft") * dfs("fft", "dac") * dfs("dac", "out")
+path2 = dfs("svr", "dac") * dfs("dac", "fft") * dfs("fft", "out")
+part2 = path1 + path2
+
+print("Part 1:", part1)
+print("Part 2:", part2)
